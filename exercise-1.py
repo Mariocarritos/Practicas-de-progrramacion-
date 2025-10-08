7ac86f377acc859cc20d5aa49c10e6945c757f0b
Mariocarritos <aaron.rondon@gmail.com>
2025-10-08 13:58:13 -0300

    Tarea uno y dos listas

---
+++ exercise-1.py
@@ -0,0 +1,14 @@
+nombre = "Aaron"
+edad = 16
+fav_game = "Deltarune"
+
+#por separado
+print (nombre)
+print (edad)
+print (fav_game)
+#con texto extra
+print ("Hola, mi nombre es", nombre,". Tengo", edad,"años. Mi juego favorito es:", fav_game)
+
+#ahora como usted lo dijo
+
+print (f"Hola, mi nombre es {nombre}. Tengo {edad} años. Mi juego favorito es: {fav_game}")
---
+++ exercise-2.py
@@ -0,0 +1,28 @@
+a = 80
+b = 5
+
+sum = a + b
+res = a - b
+mult = a * b
+
+#ahora con sus numeros
+div = a / b
+
+
+print (f"El resultado de la suma de {a} y {b} es {sum}")
+print (f"El resultado de la resta de {a} y {b} es {res}")
+print (f"El resultado de la multiplicación de {a} y {b} es {mult}")
+print (f"El resultado de la division de {a} y {b} es {div}")
+
+na = int(input("Elija un numero del 1 al 100: "))
+nb = int(input("Escoja otro numero diferente y menor al que ya escojio: "))
+
+mas = na + nb
+men = na - nb
+por = na * nb
+ent = na / nb
+
+print (f"El resultado de la suma de {na} y {nb} es {mas}")
+print (f"El resultado de la resta de {na} y {nb} es {men}")
+print (f"El resultado de la multiplicación de {na} y {nb} es {por}")
+print (f"El resultado de la división de {na} y {nb} es {ent}")
