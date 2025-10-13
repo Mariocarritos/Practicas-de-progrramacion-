63530f472b23a1bdfc7dfbbda5ce9dad6dd3e31d
Mariocarritos <aaron.rondon@gmail.com>
2025-10-13 12:25:35 -0300

    Tareas de la semana 2 listas

---
+++ w2-exercise-1.py
@@ -0,0 +1,9 @@
+nombre = input("Cual es tu nombre?: ")
+edad = int(input("Cual es tu edad?: "))
+
+if edad >= 18:
+    print(f"Hola {nombre}!. Eres mayor de edad :D")
+else:
+    print(f"Hola {nombre}!. Aun eres menor de edad :(")
+
+
---
+++ w2-exercise-2.py
@@ -0,0 +1,15 @@
+print("Quieres conocer juegos de tu genero favorito?")
+print("Escribe tu genero de videojuegos favorito y te recomendare varios del mismo genero")
+genero = input("Escoge entre: Aventura, RPG, Estrategia o Peleas: ")
+#Recuerda la mayusculas
+
+if genero == "Aventura":
+    print("Podrías probar Zelda o Hollow Knight!")
+elif genero == "RPG":
+    print("Podrías probar Undertale o EarthBound!")
+elif genero == "Estrategia":
+    print("Age of Empires y Clash Royale siempre son una buena idea!")
+elif genero == "Peleas":
+    print("Te recomiendo Street Fighter, Mortal Kombat o Dragon ball fighter Z!")
+else:
+    print("No conozco ese tipo de juegos, ¡pero suena interesante!")
